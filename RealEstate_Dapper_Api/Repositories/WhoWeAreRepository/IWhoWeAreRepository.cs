b605e22619c96e0349ffeb8aab612146d696014d
Alperen ATAKAN <alperenatakan3883@gmail.com>
2023-11-18 14:11:10 +0300

    #14son

---
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreRepository/IWhoWeAreRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.WhoWeAreRepository
+{
+    public interface IWhoWeAreRepository
+    {
+        Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync();
+        void CreateWhoWeAreDetailAsync(CreateWhoWeAreDetailDto createWhoWeAreDetailDto);
+        void DeleteWhoWeAreDetailAsync(int id);
+        void UpdateWhoWeAreDetailAsync(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto);
+        Task<GetByIDWhoWeAreDetailDto> GetWhoWeAreDetail(int id);
+    }
+}
