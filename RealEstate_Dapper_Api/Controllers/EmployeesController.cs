b01fda688dc63983d8dd4dcb5e2471a999c8b0a8
Alperen ATAKAN <alperenatakan3883@gmail.com>
2023-12-16 12:03:50 +0300

    #30

---
+++ RealEstate_Dapper_Api/Controllers/EmployeesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.EmployeeDtos;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _employeeRepository.GetAllEmployeeAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateEmployeeDto createEmployeeDto)
+        {
+            _employeeRepository.CreateEmployee(createEmployeeDto);
+            return Ok("Personel Başarılı Bir Şekilde Eklendi");
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            _employeeRepository.DeleteEmployee(id);
+            return Ok("Personel Başarıyla Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDto updateEmployeeDto)
+        {
+            _employeeRepository.UpdateEmployee(updateEmployeeDto);
+            return Ok("Personel Başarıyla Güncellendi");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var value = await _employeeRepository.GetEmployee(id);
+            return Ok(value);
+        }
+
+
+    }
+}
+
