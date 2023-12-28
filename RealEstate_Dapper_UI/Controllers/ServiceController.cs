94d8184fe6afa68b293d5add9b667d50389b255c
Alperen ATAKAN <alperenatakan3883@gmail.com>
2023-12-28 19:22:18 +0300

    #39

--- RealEstate_Dapper_UI/Controllers/ServiceController.cs
+++ RealEstate_Dapper_UI/Controllers/ServiceController.cs
@@ -7,17 +7,16 @@ namespace RealEstate_Dapper_UI.Controllers
 {
     public class ServiceController : Controller
     {
+       
         private readonly IHttpClientFactory _httpClientFactory;
-
         public ServiceController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
-
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44343/api/ServiceDetail");
+            var responseMessage = await client.GetAsync("https://localhost:44343/api/Services");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -32,37 +31,36 @@ namespace RealEstate_Dapper_UI.Controllers
         {
             return View();
         }
+
         [HttpPost]
-        public async Task<IActionResult> CreateService(CreateServiceDto createServiceDetailDto)
+        public async Task<IActionResult> CreateService(CreateServiceDto createServiceDto)
         {
+            createServiceDto.ServiceStatus = true;
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(createServiceDetailDto);
+            var jsonData = JsonConvert.SerializeObject(createServiceDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("https://localhost:44343/api/ServiceDetail", stringContent);
+            var responseMessage = await client.PostAsync("https://localhost:44343/api/Services", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
             }
             return View();
         }
-
-
         public async Task<IActionResult> DeleteService(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.DeleteAsync($"https://localhost:44343/api/ServiceDetail/{id}");
-            if (responseMessage.IsSuccessStatusCode)
+            var reponseMessage = await client.DeleteAsync($"https://localhost:44343/api/Services/{id}");
+            if (reponseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
             }
             return View();
         }
-
         [HttpGet]
         public async Task<IActionResult> UpdateService(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:44343/api/ServiceDetail/{id}");
+            var responseMessage = await client.GetAsync($"https://localhost:44343/api/Services/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -70,16 +68,14 @@ namespace RealEstate_Dapper_UI.Controllers
                 return View(values);
             }
             return View();
-
         }
-
         [HttpPost]
-        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDetailDto)
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
         {
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(updateServiceDetailDto);
+            var jsonData = JsonConvert.SerializeObject(updateServiceDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync("https://localhost:44343/api/ServiceDetail/", stringContent);
+            var responseMessage = await client.PutAsync("https://localhost:44343/api/Services/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
