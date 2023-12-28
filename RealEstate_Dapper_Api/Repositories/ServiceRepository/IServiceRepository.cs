c2e4ed0200e794c323adb2e91e09fe8202baeb33
Alperen ATAKAN <alperenatakan3883@gmail.com>
2023-12-28 16:58:28 +0300

    #38

--- RealEstate_Dapper_Api/Repositories/ServiceRepository/IServiceRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ServiceRepository/IServiceRepository.cs
@@ -5,7 +5,7 @@ namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
     public interface IServiceRepository
     {
         Task<List<ResultServiceDto>> GetAllServiceAsync();
-        void CreateService(CreateServiceDto createServiceDto);
+        void CreateServiceAsync(CreateServiceDto createServiceDto);
         void DeleteService(int id);
         void UpdateService(UpdateServiceDto updateServiceDto);
         Task<GetByIDServiceDto> GetByIDServiceAsync(int id);
